594aebb2c9425424025c4b4f7138a0ec8af91ee4
Tony Day <tony@lazylabs.com>
2017-03-21 07:10:03 +1300

    Update base_helper.rb

--- lib/bh/core_ext/rails/form/base_helper.rb
+++ lib/bh/core_ext/rails/form/base_helper.rb
@@ -75,6 +75,8 @@ module Bh
       def show_error_icon?(field_type, errors, suffix = nil)
         no_icon = %w(checkbox number_field radio_button select legend)
         hide = no_icon.include?(field_type.to_s)
+        # HACK - prevent error where @options[:errors] is nil
+        @options[:errors] ||= {}
         errors.any? && @options.fetch(:errors, {}).fetch(:icons, true) && !hide && suffix.nil?
       end
 
